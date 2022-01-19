9a185c645faf1074ff74b05a7b165e96056e71a0
ElliottZaki <spudnick99@hotmail.com>
2022-01-19 11:59:52 +0000

    Making booking class(2)

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,11 +1,26 @@
 describe Booking do
+
   describe '.create' do
     it 'makes a booking.' do
-
-
-
+      first_booking = Booking.create(check_in_date: '19/01/21', check_out_date:, space_id:)
+      #I want a booking . create to return me a booking object. With checkin, checkout and spoace ID 
+      expect(first_booking).to be_a Booking
+      expect(first_booking).to check_in_date:, check_out_date:, space_id:]
+      expect(first_booking[check_in_date]) to eq '19/01/21'
     end
   end
+
+  describe '.all' do
+    it 'returns all bookings.' do
+      #arrange
+      Booking.new(check_in_date: '19/01/21', check_out_date
+      Booking.new(check_in_date)
+      #assert
+      expect(Booking).to eq
+      
+    end
+  end
+
 end
 
 
