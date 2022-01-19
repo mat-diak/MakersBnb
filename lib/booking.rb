e31ea44726406405e6d024e723462ce03b2302e9
mikitaro-27 <matt.diak@gmail.com>
2022-01-19 12:30:53 +0000

    creating .all method for bookings

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,1 +1,26 @@
 # We wanna create a booking class here!
+
+class Booking
+  
+  @bookings = []
+
+  def self.all
+    @bookings
+  end
+  
+  def self.create(check_in_date:, check_out_date:, space_id:)
+    Booking.new(
+      check_in_date: check_in_date, 
+      check_out_date: check_out_date, 
+      space_id: space_id
+    )
+  end
+
+  attr_reader :check_in_date, :check_out_date, :space_id
+
+  def initialize(check_in_date:, check_out_date:, space_id:)
+    @check_in_date = check_in_date
+    @check_out_date = check_out_date
+    @space_id = space_id
+  end
+end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,23 +1,61 @@
-describe Booking do
+require 'booking'
 
+describe Booking do
+  
+  let(:booking) { Booking.new(check_in_date: '19/01/22', check_out_date: '25/01/22', space_id: 'some_id') }
+
+  describe '#check_in_date' do
+    it 'returns the check_in date' do
+      expect(booking.check_in_date).to eq '19/01/22'
+    end
+  end
+  describe '#check_out_date' do
+    it 'returns the check_out date' do
+      expect(booking.check_out_date).to eq '25/01/22'
+    end
+  end
+  describe '#space_id' do
+    it 'returns the space_id' do
+      expect(booking.space_id).to eq 'some_id'
+    end
+  end
+  
   describe '.create' do
     it 'makes a booking.' do
-      first_booking = Booking.create(check_in_date: '19/01/21', check_out_date:, space_id:)
+      #Arrange
+      # nothing to arrange
+      #Act
+      first_booking = Booking.create(check_in_date: '19/01/22', check_out_date: '25/01/22', space_id: 'some_id')
+      #Assert
       #I want a booking . create to return me a booking object. With checkin, checkout and spoace ID 
       expect(first_booking).to be_a Booking
-      expect(first_booking).to check_in_date:, check_out_date:, space_id:]
-      expect(first_booking[check_in_date]) to eq '19/01/21'
+      expect(first_booking.check_in_date).to eq '19/01/22'
+      expect(first_booking.check_out_date).to eq '25/01/22'
+      expect(first_booking.space_id).to eq 'some_id'
+      expect(Booking.all).to include first_booking
     end
   end
 
   describe '.all' do
     it 'returns all bookings.' do
       #arrange
-      Booking.new(check_in_date: '19/01/21', check_out_date
-      Booking.new(check_in_date)
+      # I added two bookings.
+      first_booking = Booking.create(
+        check_in_date: '25/12/22',
+        check_out_date: '27/12/22',
+        space_id: 'id_1'
+      )
+      second_booking = Booking.create(
+        check_in_date: '12/11/22',
+        check_out_date: '14/11/22',
+        space_id: 'id_2'
+      )
+      #act
+      # call Booking.all
       #assert
-      expect(Booking).to eq
-      
+      # I want Booking.all to have all created bookings
+      expect(Booking.all.length).to be 2
+      expect(Booking.all).to include first_booking, second_booking
     end
   end
 
